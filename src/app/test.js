c3c8942998a2e09dc3916daa189d3542060d655d
Johnny Pesola <johnny@pesola.eu>
2016-03-26 21:38:42 +0100

    Basic project setup, added TSlint and rules

---
+++ src/app/test.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return 'Hello ' + person;
+}
+var user = 'Jane User';
+var result;
+result = greeter(user);
---
+++ src/app/test.ts
@@ -0,0 +1,16 @@
+
+/**
+ * greeter - description
+ *
+ * @param  {type} person description
+ * @return {type}        description
+ */
+function greeter(person: string) {
+
+    return 'Hello ' + person;
+}
+
+const user = 'Jane User';
+let result: string;
+
+result = greeter(user);
