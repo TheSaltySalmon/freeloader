1e628e617a65f42be24370dcee31598d4b7d2062
johnnypesola <johnny@pesola.eu>
2017-09-07 00:07:51 +0200

    17 - Refactored to Controllers and Services

--- freeloader/Assets/Scripts/Backgrounds/BackgroundScroll.cs
+++ freeloader/Assets/Scripts/Backgrounds/BackgroundScroll.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour {
 
-    private GameObject _playerShip;
+    private GameObject _player;
 
     public float yOffset;
     public float xOffset;
@@ -13,14 +13,14 @@ public class BackgroundScroll : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        _playerShip = (FindObjectOfType(typeof(PlayerShipMovement)) as PlayerShipMovement).gameObject;
+        _player = (FindObjectOfType(typeof(PlayerController)) as PlayerController).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xPos = _playerShip.transform.position.x / scrollFactor + xOffset;
-        float yPos = _playerShip.transform.position.y / scrollFactor + yOffset;
+        float xPos = _player.transform.position.x / scrollFactor + xOffset;
+        float yPos = _player.transform.position.y / scrollFactor + yOffset;
 
         transform.position = new Vector3(xPos, yPos, transform.position.z);
     }
